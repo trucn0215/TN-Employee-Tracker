6e57746ca40ad6287bb81f2291ef598d5a8fddc7
Truc Nguyen <trucn0215@gmail.com>
2020-12-27 22:21:28 -0800

    change database name to match with database name created in sql

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "password",
-    database: "employee"
+    database: "employeeDB"
 });
 
 connection.connect((err) => {
