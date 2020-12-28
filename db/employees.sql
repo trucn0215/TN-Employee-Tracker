ffbcd19d264cd08b166250cdc051cdec5464ee8d
Truc Nguyen <trucn0215@gmail.com>
2020-12-27 21:03:59 -0800

    add columns for role table and employee table

--- db/employees.sql
+++ db/employees.sql
@@ -1,17 +1,21 @@
 CREATE DATABASE employeeDB;
-
 USE employeeDB;
-
 CREATE TABLE departments (
     id INT AUTO_INCREMENT;
     name VARCHAR(30);
     PRIMARY KEY (id);
 );
-
 CREATE TABLE roles (
-
+    department_id INT AUTO_INCREMENT;
+    title VARCHAR(30);
+    salary DECIMAL;
+    PRIMARY KEY (department_id);
 );
 
 CREATE TABLE employees (
-
+    manager_id INT AUTO_INCREMENT;
+    first_name VARCHAR(30);
+    last_name VARCHAR(30);
+    role_id INT;
+    PRIMARY KEY (manager_id);
 );
