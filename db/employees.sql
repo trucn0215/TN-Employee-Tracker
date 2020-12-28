490bf387a55bfbe34f74a2fe1e490bd40be976b5
Truc Nguyen <trucn0215@gmail.com>
2020-12-27 20:54:37 -0800

    add columns id and name for departments table

---
+++ db/employees.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT;
+    name VARCHAR(30);
+    PRIMARY KEY (id);
+);
+
+CREATE TABLE roles (
+
+);
+
+CREATE TABLE employees (
+
+);
