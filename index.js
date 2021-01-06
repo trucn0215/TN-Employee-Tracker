b6ddfd7fbd8d7e95d3fabb29021713eb50f22088
Truc Nguyen <trucn0215@gmail.com>
2021-01-05 21:16:21 -0800

    add additional functions block following choices option list

--- index.js
+++ index.js
@@ -61,23 +61,38 @@ function startingPromt() {
                     break;
 
                 case "4. Add Department":
-                    console.log("\n\n---ADD YOUR NEW DEPARTMENT!---\n\n");
+                    console.log("\n\n---ADD A DEPARTMENT!---\n\n");
                     add_Department();
                     break;
 
-                case "5. Add Roles":
-                    console.log("\n\n---ADD YOUR NEW ROLE!---\n\n");
+                case "5. Delete Department":
+                    console.log("\n\n---DELETE DEPARTMENT!---\n\n");
+                    remove_department();
+                    break;
+
+                case "6. Add Roles":
+                    console.log("\n\n---ADD NEW ROLE!---\n\n");
                     add_Role();
                     break;
 
-                case "6. Add Employee":
-                    console.log("\n\n---ADD YOUR NEW EMPLOYEE!---\n\n");
+                case "7. Delete Role":
+                    console.log("\n\n---DELETE ROLE!---\n\n");
+                    remove_Role();
+                    break;
+
+                case "8. Add Employee":
+                    console.log("\n\n---ADD NEW EMPLOYEE!---\n\n");
                     add_Employee();
                     break;
 
-                case "7. Update Employee Role":
-                    console.log("\n\n---UPDATE YOUR EMPLOYEE ROLE!---\n\n");
-                    update_Employee_Role();
+                case "9. Delete Employee":
+                    console.log("\n\n---DELETE EMPLOYEE!---\n\n");
+                    remove_Employee();
+                    break;
+
+                case "10. Update Employee":
+                    console.log("\n\n---UPDATE EMPLOYEE!---\n\n");
+                    update_Employee();
                     break;
 
                 default:
@@ -87,7 +102,7 @@ function startingPromt() {
         })
 }
 
-// VIEW DEPARTMENT TABLE FUNCTION
+// 1. VIEW DEPARTMENT TABLE FUNCTION
 function view_Department_Table() {
     db
         .getDepartments()
@@ -99,7 +114,7 @@ function view_Department_Table() {
         })
 }
 
-// VIEW ROLE TABLE FUNCTION
+// 2. VIEW ROLE TABLE FUNCTION
 function view_Role_Table() {
     db
         .getRole()
@@ -111,7 +126,7 @@ function view_Role_Table() {
         })
 }
 
-// VIEW EMPLOYEE TABLE FUNCTION
+// 3. VIEW EMPLOYEE TABLE FUNCTION
 function view_Employee_Table() {
     db
         .getEmployee()
@@ -123,7 +138,7 @@ function view_Employee_Table() {
         })
 }
 
-// ADDING DEPARTMENT FUNCTION
+// 4. ADDING DEPARTMENT FUNCTION
 function add_Department() {
     // console.log("Adding a Department");
     inquirer
@@ -142,7 +157,12 @@ function add_Department() {
         })
 }
 
-// ADDING ROLE FUNCTION
+// 5. REMOVE DEPARTMENT
+function remove_department(){
+
+}
+
+// 6. ADDING ROLE FUNCTION
 function add_Role() {
     // console.log("Adding a Role");
     db
@@ -190,11 +210,14 @@ function add_Role() {
         })
 }
 
-// ADDING EMPLOYEE FUNCTION
-function add_Employee() {
-    // console.log("Adding a Empployee");
+// 7. REMOVE ROLE
+function remove_Role(){
 
-    // Select Department THEN list the roles in that departments
+}
+
+// 8. ADDING EMPLOYEE FUNCTION
+function add_Employee() {
+
     db
         .getDepartments()
         .then((departmentData) => {
@@ -214,7 +237,6 @@ function add_Employee() {
                 .then((response) => {
 
                     const departmentDataPick = response.departmentChoices_id;
-
 
                     db
                         .getRole(departmentDataPick)
@@ -261,10 +283,15 @@ function add_Employee() {
                                 })
                         })
                 })
-
         })
 }
 
-function update_Employee_Role() {
+// 9. REMOVE EMPLOYEE
+function remove_Employee(){
+
+}
+
+// 10. Update Employee
+function update_Employee() {
     // startingPromt();
 }
